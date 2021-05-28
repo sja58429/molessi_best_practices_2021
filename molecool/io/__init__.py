3bfadef9e6fa5508326281da6e4012bd3d326321
Steven <steven.ayoub.362@my.csun.edu>
2021-05-28 10:27:04 -0700

    added an IO directory

---
+++ molecool/io/__init__.py
@@ -0,0 +1,7 @@
+"""
+init for IO subpackage
+"""
+from .pdb import open_pdb
+from .xyz import write_xyz
+
+import molecool.io
---
+++ molecool/io/xyz.py
@@ -0,0 +1,16 @@
+"""
+Functions for reading and writing xyz files 
+"""
+
+def write_xyz(file_location, symbols, coordinates):
+    
+    # Write an xyz file given a file location, symbols, and coordinates.
+    num_atoms = len(symbols)
+    
+    with open(file_location, 'w+') as f:
+        f.write('{}\n'.format(num_atoms))
+        f.write('XYZ file\n')
+        
+        for i in range(num_atoms):
+            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], 
+                                              coordinates[i,0], coordinates[i,1], coordinates[i,2]))
